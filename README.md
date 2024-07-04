7b8b420fa266aab6d793fa975a57185dc839fb43
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-07-04 14:57:42 +0200

    init: add initial config

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center">.dotfiles</h1>
+
+Managed using GNU Stow.
+
+## How to use it
+
+1. Clone this repo:
+
+   ```shell
+   git clone --recursive https://github.com/EdwardKerckhof/.dotfiles.git $HOME/.dotfiles-edward
+   cd $HOME/.dotfiles-edward
+   ```
+
+2. Install GNU Stow:
+
+   ```shell
+   # On Arch Linux
+   sudo pacman -S stow
+   ```
+
+3. Uncomment the lines in [`install.sh`](./install.sh) of the dotfiles to install
+4. Execute `./install.sh`.
+   Make sure that you run the script in the main dotfiles directory.
+   I.e change directory to `.dotfiles-edward` first.
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function check_prog() {
+	if ! hash "$1" >/dev/null 2>&1; then
+		echo "Command not found: $1. Aborting..."
+		exit 1
+	fi
+}
+
+check_prog stow
+check_prog curl
+
+mkdir -p "$HOME/.config"
+
+############################# How to use it #############################
+#                                                                       #
+# Uncomment the lines of the configs you want to install below.         #
+# Then run this script from within the dotfiles directory.              #
+# E.g. `cd ~/.dotfiles; ./install.sh`                                   #
+#                                                                       #
+# To uninstall the config later, run stow -D in the dotfiles directory. #
+# E.g. `cd ~/.dotfiles; stow -D vim`                                    #
+#                                                                       #
+#########################################################################
+
+#stow zsh
+#stow kitty
+#stow nvim
