cb1c42597550ab6d67fd13f6d17bd32a273cddca
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-10-08 21:27:32 +0200

    feat: emoji picket

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
         tailwindcss = {},
         cssls = { settings = { css = { lint = { unknownAtRules = "ignore" } } } },
