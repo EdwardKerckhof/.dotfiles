084d46a4b15335896e4313b318214269061f51d0
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-11-25 19:18:19 +0100

    feat: kitty and sketchybar updates

--- sketchybar/.config/sketchybar/plugins/calendar.sh
+++ sketchybar/.config/sketchybar/plugins/calendar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sketchybar --set "$NAME" label="$(date +'%a %d %b %H:%M:%S')"
+# sketchybar --set "$NAME" label="$(date +'%a %d %b %H:%M:%S')"
+sketchybar --set "$NAME" icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
