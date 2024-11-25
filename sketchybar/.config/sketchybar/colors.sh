385303673502602c8526968e7c5a6d669b35e6be
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-11-25 17:46:31 +0100

    feat: updates

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 export WHITE=0xffffffff
+
+# Color Palette
+BLACK=0xff181926
+RED=0xffed8796
+GREEN=0xffa6da95
+BLUE=0xff8aadf4
+YELLOW=0xffeed49f
+ORANGE=0xfff5a97f
+MAGENTA=0xffc6a0f6
+GREY=0xff939ab7
+TRANSPARENT=0x00000000
 
 # -- Default Scheme --
 #export BAR_COLOR=0x40000000
