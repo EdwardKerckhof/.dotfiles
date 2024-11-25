1c41564faea019e576ef0a92688360f5aedf8cd9
EdwardKerckhof <edwardkerckhof@hotmail.com>
2024-11-25 19:47:29 +0100

    feat: aerospace and sketchybar

--- sketchybar/.config/sketchybar/items/aerospace.sh
+++ sketchybar/.config/sketchybar/items/aerospace.sh
@@ -2,23 +2,65 @@
 
 sketchybar --add event aerospace_workspace_change
 
-for sid in $(aerospace list-workspaces --all); do
-  sketchybar --add item space."$sid" left \
-    --subscribe space."$sid" aerospace_workspace_change \
-    --set space."$sid" \
-    icon="$sid" \
-    label.font="sketchybar-app-font:Regular:16.0" \
-    click_script="aerospace workspace $sid" \
-    script="$PLUGIN_DIR/aerospace.sh $sid"
-  # label.padding_right=20 \
-  # label.y_offset=-1 \
+for m in $(aerospace list-monitors | awk '{print $1}'); do
+  for i in $(aerospace list-workspaces --monitor $m); do
+    sid=$i
+    space=(
+      space="$sid"
+      icon="$sid"
+      icon.highlight_color=$RED
+      icon.padding_left=10
+      icon.padding_right=0
+      display=$m
+      padding_left=2
+      padding_right=2
+      label.padding_right=20
+      label.color=$GREY
+      label.highlight_color=$WHITE
+      label.font="sketchybar-app-font:Regular:16.0"
+      label.y_offset=-1
+      background.color=$BACKGROUND_1
+      background.border_color=$BACKGROUND_2
+      click_script="aerospace workspace $sid"
+      script="$PLUGIN_DIR/aerospace.sh"
+    )
+
+    sketchybar --add space space.$sid left \
+      --set space.$sid "${space[@]}" \
+      --subscribe space.$sid aerospace_workspace_change
+
+    apps=$(aerospace list-windows --workspace $sid | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+
+    icon_strip=" "
+    if [ "${apps}" != "" ]; then
+      while read -r app; do
+        icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+      done <<<"${apps}"
+    else
+      icon_strip=" —"
+    fi
+
+    sketchybar --set space.$sid label="$icon_strip"
+  done
+
+  for i in $(aerospace list-workspaces --monitor $m --empty); do
+    sketchybar --set space.$i display=0
+  done
 done
 
-sketchybar --add item space_separator left \
-  --set space_separator icon="􀆊" \
-  icon.color="$ACCENT_COLOR" \
-  icon.padding_left=4 \
-  label.drawing=off \
-  background.drawing=off
-# script="$PLUGIN_DIR/space_windows.sh" \
-# --subscribe space_separator space_windows_change
+space_creator=(
+  icon=􀆊
+  icon.font="$FONT:Heavy:16.0"
+  padding_left=10
+  padding_right=8
+  label.drawing=off
+  display=active
+  #click_script='yabai -m space --create'
+  script="$PLUGIN_DIR/space_windows.sh"
+  #script="$PLUGIN_DIR/aerospace.sh"
+  icon.color=$WHITE
+)
+
+sketchybar --add item space_creator left \
+  --set space_creator "${space_creator[@]}" \
+  --subscribe space_creator aerospace_workspace_change
--- sketchybar/.config/sketchybar/plugins/calendar.sh
+++ sketchybar/.config/sketchybar/plugins/calendar.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# sketchybar --set "$NAME" label="$(date +'%a %d %b %H:%M:%S')"
-sketchybar --set "$NAME" icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+sketchybar --set "$NAME" label="$(date +'%a %d %b %H:%M:%S')"
